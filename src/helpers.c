b598c23cd8530b172abfcf58dd2f011fa130e578
Pedro Vanzella <pedro@pedrovanzella.com>
2012-06-10 20:09:02 -0300

    I hope I don't ever have to try to understand this

--- src/helpers.c
+++ src/helpers.c
@@ -19,6 +19,32 @@ void parse_memsize(char* buffer, int* size)
 
 void parse_procsize(char* buffer, int* id, int* size)
 {
+	char i[4];
+	char s[4];
+	char found_id = 0;
+	char last_char_was_digit = 0;
+	int pos_i = 0;
+	int pos_s = 0;
+
+	while (*buffer) {
+		if (last_char_was_digit && ((*buffer == ' ') || (*buffer == '\t'))) {
+			found_id = 1;
+		}
+		if (!found_id) {
+			if ((*buffer >= '0') && (*buffer <= '9')) {
+				last_char_was_digit = 1;
+				i[pos_i++] = *buffer;
+			}
+		} else { // Já temos ID, achar size
+			if ((*buffer >= '0') && (*buffer <= '9')) {
+				s[pos_s++] = *buffer;
+			}
+		}
+		buffer++;
+	}
+
+	*id = atoi(i);
+	*size = atoi(s);
 }
 
 void parse_read(char* buffer, int* page, int* id)
