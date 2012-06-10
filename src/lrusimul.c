4b2af6308d391b4785e21de2e0ba573f7bf290de
Pedro Vanzella <pedro@pedrovanzella.com>
2012-06-10 15:18:19 -0300

    opening file

--- src/lrusimul.c
+++ src/lrusimul.c
@@ -1,8 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "../include/fifo.h"
 
 int main(int argc, char* argv[])
 {
+	if (argc != 2) {
+		fprintf(stderr, "[-] Error: usage: %s config_file\n", argv[0]);
+		exit(1);
+	}
+
+	char* filename = argv[1];
+
+	FILE* fp = fopen(filename, "r");
+	if (!fp) {
+		fprintf(stderr, "[-] Error: failed to open file %s\n", filename);
+		exit(1);
+	}
+
 	return 1;
 }
