334a4a3ae408782a8c1b910e72f318ffb69fe521
Pedro Vanzella <pedro@pedrovanzella.com>
2012-06-11 16:45:05 -0300

    garantir que as páginas comecem nulas

--- src/memory.c
+++ src/memory.c
@@ -9,5 +9,10 @@ struct memory* make_memory(int size)
 	m->size = size;
 	m->pages = (struct page**)malloc(size * sizeof(struct page*));
 
+	int i;
+	for (i = 0; i < size; i++) {
+		m->pages[i] = NULL; // Garantir que as páginas sejam nulas no início
+	}
+
 	return m;
 }
